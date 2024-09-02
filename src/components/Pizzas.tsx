dc425826735497ccfe4cabe018326e0e8e98e267
AdolfKing20 <rodrigogfxs@gmail.com>
2024-09-01 23:59:39 -0300

    update

---
+++ src/components/Pizzas.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Pizzas() {
+  return (
+    <section className='w-full'>
+        <section className='gridcontainer'>
+        <div className='w-full h-24 bg-slate-400'>
+        </div>
+        <div className='w-full  bg-slate-400'>
+        </div>
+        <div className='w-full  bg-slate-400'>
+        </div>
+        <div className='w-full  bg-red-400'>
+        </div>
+        <div className='w-full  bg-slate-400'>
+        </div>
+        </section>
+    </section>
+  )
+}
+
+export default Pizzas
