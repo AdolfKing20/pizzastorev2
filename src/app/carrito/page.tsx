4ae62ffee7bd190088ced6d9a2cad5ccbd8871cd
AdolfKing20 <rodrigogfxs@gmail.com>
2024-09-03 15:49:55 -0300

    prueba

---
+++ src/app/carrito/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Aca va la lista de productos del carrito</div>
+  )
+}
+
+export default page
---
+++ src/app/contacto/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Aca va la pagina de contacto</div>
+  )
+}
+
+export default page
